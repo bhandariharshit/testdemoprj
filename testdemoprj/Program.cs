ce79ab62ddbf18e39e2a2f763f0aeccce9fcadcf
Harshit <test@test.com>
2020-09-24 20:44:30 +0530

    Add project files.

---
+++ testdemoprj/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Configuration;
+
+namespace testdemoprj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string email = ConfigurationManager.AppSettings["email"];
+            Console.WriteLine("hello world");
+            Console.ReadLine();
+        }
+
+        public void sum()
+        {
+            Program p;
+        }
+    }
+}
---
+++ testdemoprj/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("testdemoprj")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("testdemoprj")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e3748f68-5f1a-481d-9de6-bc50587b3135")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
